95d67f5948007f0736cd7e94863ed25cb4859339
EmekaManuel <marrnuel12@gmail.com>
2023-03-06 01:05:10 +0100

    linked the home page shop-now  button to the shop page

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HeroImage from "../assets/Rectangle 1.png";
-
+import { Link } from "react-router-dom";
 const Hero = () => {
   return (
 
@@ -15,7 +15,7 @@ const Hero = () => {
     {/*******************main hero *********************/}
       <div className="absolute top-[10px] md:top-[100px] w-full h-full flex flex-col text-center text-white p-6">
         
-        <h1 className="text-[35px] md:text-[60px] heroTopic p-5 md:p-2">new season arrivals</h1>
+        <h1 className="text-[35px] md:text-[60px] heroTopic  p-5 md:p-2">new season arrivals</h1>
         <h1 className=" hero-paragraph opacity-75 text-[20px]">
           Lorem ipsum dolor sit amet consectetur 
           <br />
@@ -24,9 +24,10 @@ const Hero = () => {
           Lorem ipsum dolor sit amet 
         </h1>
 
-        <div className="flex mt-4 justify-center items-center mx-auto p-1 max-w-[700px] w-full"
-        >
-          <button className="shop-now px-[70px] py-3 bg-white text-black rounded hover:bg-gray-100">Shop Now</button>
+        <div className="flex mt-4 justify-center items-center mx-auto p-1 max-w-[700px] w-full">
+          <button className="shop-now px-[70px] py-3 bg-white text-black rounded hover:bg-gray-100">
+            <Link to = "/shop">   Shop Now</Link>
+            </button>
         </div>
       </div>
     </div>
